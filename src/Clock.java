e308cea3593e573aa5ad7fb0cd9b7ead10d23345
Robin Reuteler <reutelerr@hotmail.com>
2020-02-27 14:03:32 +0100

    init

---
+++ src/Clock.java
@@ -0,0 +1,25 @@
+public abstract class Clock
+{
+    private java.util.ArrayList<ClockViewer> observers;
+
+    public void notifyObservers()
+    {
+
+    }
+
+    public void attach(ClockViewer v)
+    {
+
+    }
+
+    public void detach(ClockViewer v)
+    {
+
+    }
+
+}
+
+public class Chronometer extends Clock
+{
+    private 
+}
